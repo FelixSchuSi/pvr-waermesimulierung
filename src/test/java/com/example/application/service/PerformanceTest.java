cc2e96afd0ff43e271c450a12a2eaa922172fe6b
Jannik Bergjan <jannik@bergjan.com>
2022-06-25 18:35:08 +0200

    Disable inital PerformanceTest

--- src/test/java/com/example/application/service/PerformanceTest.java
+++ src/test/java/com/example/application/service/PerformanceTest.java
@@ -4,6 +4,7 @@ import com.example.application.entity.BaseConfigEntity;
 import helper.CsvReport;
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -20,7 +21,7 @@ public class PerformanceTest {
     private final SimulationServiceFromConfigService serviceFromConfig = new SimulationServiceFromConfigService();
 
     @Test
-    @Ignore
+    @Disabled
     void runAll() {
         List<String> numberStrings = IntStream.range(0, TEST_RERUN_COUNT).boxed().map(Object::toString).collect(Collectors.toList());
         List<String> columns = new ArrayList<>(List.of("runName", "threadCount", "implementationStrategy"));
